dab649917042319b12bcab9ac472ccfb9127c590
TimoteoGambella <timi.gambella@hotmail.com>
2023-07-15 23:54:48 -0300

    ...

--- app.js
+++ app.js
@@ -14,7 +14,13 @@ const uri = `mongodb+srv://timoteogambella:${password}@cluster0.iqq7wac.mongodb.
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static(__dirname+"/public"))
-app.use(cors())
+
+const corsOptions ={
+    origin:'*', 
+    credentials:true,        
+    optionSuccessStatus:200,
+}
+app.use(cors(corsOptions))
 
 const CryptoJS = require('crypto-js');
 
